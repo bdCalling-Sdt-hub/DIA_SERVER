2713fa09be8c8fd807ac0cf88f032130cc9e73ae
Sumaiya <sumaiya.16.ete.cuet@gmail.com>
2023-12-18 16:34:04 +0600

    validated image add successfully

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->integer('is_verified')->default(0);
             $table->string('password');
+            $table->string('image')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
